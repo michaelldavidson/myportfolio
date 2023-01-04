c2057612229b99048ba25653923677c0373ee5f9
Michael Davidson <michaelldavidson@gmail.com>
2023-01-04 20:49:49 +0800

    Add skeleton for skills and styling fix projects

--- scripts.js
+++ scripts.js
@@ -34,13 +34,31 @@ let htmlButton = document.getElementById('html-button');
 htmlButton.addEventListener('click', showHTML);
 
 function showHTML() {
+
+    //hides all other buttons - but do I want this? how can I make it a toggle on/off button?
+    hideElement();
+    buttonsBack();
     const allHTML = document.getElementsByClassName ('html-tag');
     for (const stream of allHTML) {
                     stream.style.display = '';
     }
     htmlButton.style.backgroundColor = '#ED535A';
     htmlButton.style.boxShadow = '0px 0px';
+    htmlButton.addEventListener('click', hideHTML);
 };
+
+// Why doesn't this work?
+
+/* function hideHTML() {
+    const allHTML = document.getElementsByClassName ('html-tag');
+    for (const stream of allHTML) {
+                    stream.style.display = 'none';
+    }
+    htmlButton.style.backgroundColor = '';
+    htmlButton.style.boxShadow = '';
+    htmlButton.addEventListener('click', showHTML); 
+
+}*/
 
 // displays all the Scratch tagged boxes
 
@@ -49,6 +67,8 @@ let scratchButton = document.getElementById('scratch-button');
 scratchButton.addEventListener('click', showScratch);
 
 function showScratch() {
+    hideElement();
+    buttonsBack();
     const allScratch = document.getElementsByClassName ('scratch-tag');
     for (const stream of allScratch) {
                     stream.style.display = '';
@@ -64,6 +84,8 @@ let spreadsheetButton = document.getElementById('spreadsheet-button');
 spreadsheetButton.addEventListener('click', showSpreadsheet);
 
 function showSpreadsheet() {
+    hideElement();
+    buttonsBack();
     const allSpreadsheet = document.getElementsByClassName ('spreadsheet-tag');
     for (const stream of allSpreadsheet) {
                     stream.style.display = '';
